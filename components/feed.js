a21d2c21e55f942b1c9ee43b6a92e61e751b672b
PHONGRAWEE YAEMMISRI <phongrawee_y@cmu.ac.th>
2020-08-29 15:33:41 +0700

    add overlay and bid in feed screen

--- components/feed.js
+++ components/feed.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text, FlatList } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import firebase from "../database/firebase";
 import { Container, Footer, FooterTab, Button, Icon } from "native-base";
+import Overlay from "react-native-modal-overlay";
+import { TextInput } from "react-native-gesture-handler";
 export default class Feed extends Component {
+  state = {
+    modalVisible: false,
+  };
   constructor() {
     super();
     this.state = {
       displayName: firebase.auth().currentUser.displayName,
       uid: firebase.auth().currentUser.uid,
       list: [],
-      list2: [],
+      ordername: "",
+      bid: '',
+      orderkey: "",
     };
   }
+  updateInputVal = (val, prop) => {
+    const state = this.state;
+    state[prop] = val;
+    this.setState(state);
+  };
+  onClose = () => this.setState({ modalVisible: false });
 
   signOut = () => {
     firebase
@@ -38,10 +57,10 @@ export default class Feed extends Component {
           li.push({
             key: child.key,
             name: child.val().name,
-            email : child.val().email,
+            email: child.val().email,
             Jacket: child.val().Jacket,
             Shorts: child.val().Shorts,
-            TShirt: child.val().TShirt,
+            Tshirt: child.val().Tshirt,
             orderDropdatetime: child.val().orderDropdatetime,
             orderPickdatetime: child.val().orderPickdatetime,
             orderdate: child.val().orderdate,
@@ -51,13 +70,37 @@ export default class Feed extends Component {
         this.setState({ list: li });
       });
   }
-
+  bidpress = () => {
+    this.setState({ modalVisible: true });
+  };
+  setname(input, key) {
+    this.setState({ ordername: input })
+    this.setState({ orderkey: key });
+  }
+  setbid(input, key) {
+    this.setname(input, key);
+    this.bidpress();
+  }
+  setbidinput(Lname,ordername, bid,key) {
+    var dbA = firebase.database().ref("BidOrder");
+    var userid = dbA.child(key);
+    userid
+      .set({
+        'Laundry': Lname,
+        'customer': ordername,
+        'Bidamount': bid,
+      })
+      .then((data) => {
+        console.log("data", data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <Container>
-        <View
-          style={{ flex: 1, alignSelf: "center", justifyContent: "center" }}
-        >
+        <View style={styles.container}>
           <FlatList
             style={{ width: "100%" }}
             data={this.state.list}
@@ -65,9 +108,42 @@ export default class Feed extends Component {
             renderItem={({ item }) => {
               return (
                 <View>
-                  <Text>
-                    {item.key} {item.email}{item.address} 
-                    </Text>
+                  <TouchableOpacity
+                    style={styles.container}
+                    onPress={() => this.setbid(item.name, item.key)}
+                  >
+                    <Text>User : {item.name}</Text>
+                    <Text>Amount of clothes</Text>
+                    <Text>Jacket x{item.Jacket}</Text>
+                    <Text>T-Shirt x{item.Tshirt}</Text>
+                    <Text>Shorts x{item.Shorts}</Text>
+                    <Text>Drop Time : {item.orderDropdatetime}</Text>
+                    <Text>Pick Time : {item.orderPickdatetime}</Text>
+                    <Text>Address : {item.address}</Text>
+                  </TouchableOpacity>
+                  <Overlay
+                    visible={this.state.modalVisible}
+                    onClose={this.onClose}
+                    closeOnTouchOutside
+                  >
+                    <Text>User : {this.state.ordername}</Text>
+                    <Text>Input Bid :</Text>
+                    <TextInput
+                      value={this.state.bid}
+                      onChangeText={(val) => this.updateInputVal(val, "bid")}
+                    >
+                     
+                    </TextInput>
+                    <Button
+                      title="Bid"
+                      style={styles.NextButton}
+                      onPress={() =>
+                        this.setbidinput(this.state.displayName,this.state.ordername, this.state.bid,this.state.orderkey)
+                      }
+                    >
+                  <Text>OK</Text>
+                    </Button>
+                  </Overlay>
                 </View>
               );
             }}
@@ -108,11 +184,17 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    padding: 35,
+    padding: 10,
     backgroundColor: "#fff",
   },
   textStyle: {
     fontSize: 15,
     marginBottom: 20,
+  },
+  NextButton: {
+    width: 30,
+    paddingTop: 10,
+    alignItems: "center",
+    marginLeft: 150,
   },
 });
